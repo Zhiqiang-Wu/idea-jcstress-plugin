842426379e17fed7352ad48766231eded43e19d0
Zhiqiang-Wu <755960141@qq.com>
2022-08-24 17:42:06 +0800

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,17 +13,18 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2021.3")
+    version.set("2022.2")
     type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+    plugins.set(listOf("com.intellij.java"))
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+        options.encoding = "UTF-8"
     }
 
     patchPluginXml {
---
+++ src/main/java/wzq/jcstress/plugin/configuration/JCStressRunLineMarkerContributor.java
@@ -0,0 +1,38 @@
+package wzq.jcstress.plugin.configuration;
+
+import com.intellij.execution.lineMarker.RunLineMarkerContributor;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.Function;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static com.intellij.icons.AllIcons.Actions.ProfileYellow;
+
+/**
+ * @author 吴志强
+ * @date 2022/8/24
+ */
+public class JCStressRunLineMarkerContributor extends RunLineMarkerContributor {
+
+    @Override
+    public @Nullable Info getInfo(@NotNull PsiElement psiElement) {
+        if (psiElement instanceof PsiClass psiClass) {
+            if (psiClass.hasAnnotation("org.openjdk.jcstress.annotations.JCStressTest")) {
+                final AnAction[] actions = new AnAction[]{ActionManager.getInstance().getAction("RunClass")};
+                return new Info(ProfileYellow, new TooltipProvider(), actions);
+            }
+        }
+        return null;
+    }
+
+    private static class TooltipProvider implements Function<PsiElement, String> {
+
+        @Override
+        public String fun(PsiElement psiElement) {
+            return "";
+        }
+    }
+}
