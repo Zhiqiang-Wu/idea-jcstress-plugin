de0256baead5e84041578101be61ca3444c4eeeb
Zhiqiang-Wu <755960141@qq.com>
2022-08-24 19:47:10 +0800

    init

---
+++ src/main/java/wzq/jcstress/plugin/configuration/JCStressConfigurationType.java
@@ -0,0 +1,26 @@
+package wzq.jcstress.plugin.configuration;
+
+import com.intellij.execution.configurations.JavaRunConfigurationModule;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.execution.configurations.SimpleConfigurationType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.NotNullLazyValue;
+import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.icons.AllIcons.Actions.ProfileYellow;
+
+/**
+ * @author 吴志强
+ * @date 2022/8/24
+ */
+public class JCStressConfigurationType extends SimpleConfigurationType {
+
+    public JCStressConfigurationType() {
+        super("jcstress-id", "JCStress", "Configuration to run a jcstress test", NotNullLazyValue.createValue(() -> ProfileYellow));
+    }
+
+    @Override
+    public @NotNull RunConfiguration createTemplateConfiguration(@NotNull Project project) {
+        return new JCStressRunConfiguration("jcstress", new JavaRunConfigurationModule(project, false), this);
+    }
+}
